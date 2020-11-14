b2f4ebd4bac4d379e34e9effc22c8e5f65040a0e
ninonb <ninonbecquart@gmail.com>
2020-11-14 13:33:17 -0800

    changing init kwargs, return for sub
    oops-- missed this stuff before

--- src/autodiff/AD_Object.py
+++ src/autodiff/AD_Object.py
@@ -1,17 +1,11 @@
-
 class Var:
     """
     TODO: docs
     """
-    def __init__(self, val, **kwargs):
+    def __init__(self, val, der = 1):
         self.val = val
-        if 'derivative' in kwargs:
-            self.der = kwargs['derivative']
-        else:
-            self.der = 1
-        self.args = kwargs
+        self.der = der
         
-
     def __add__(self, other):
         try:
             new_val = self.val + other.val
@@ -20,7 +14,7 @@ class Var:
             new_val = self.val + other
             new_der = self.der + other
 
-        return AutoDiffToy(new_val, new_der)
+        return Var(new_val, new_der)
 
     def __radd__(self, other):
         return self.__add__(other)
@@ -32,12 +26,14 @@ class Var:
         except:
             new_val = self.val - other
             new_der = self.der - other
+            
+        return Var(new_val, new_der)
 
     def __rsub__(self, other):
         return self.__sub__(other)
 
     def __mul__(self, other):
-       try:
+        try:
             new_val = self.val * other.val
             new_der = self.der * other.val + self.val * other.der
         except:
