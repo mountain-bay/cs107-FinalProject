c08711c27d6f4b68198fb9cbd3d7bac176d00d6a
zacharyellison <zellison@college.harvard.edu>
2020-11-16 08:38:29 -0700

    fix merge and revert changes, include operation overload from Ninon

--- src/autodiff/AD_Object.py
+++ src/autodiff/AD_Object.py
@@ -14,23 +14,42 @@ class Var:
     def __repr__(self):
         return f"Var(val={self.val}, der={self.der})"
 
-    def __add__(self, other):
-        raise NotImplementedError
+        def __add__(self, other):
+        try:
+            new_val = self.val + other.val
+            new_der = self.der + other.der
+        except:
+            new_val = self.val + other
+            new_der = self.der + other
+        return Var(new_val, new_der)
 
     def __radd__(self, other):
-        raise NotImplementedError
+        return self.__add__(other)
 
     def __sub__(self, other):
-        raise NotImplementedError
+        try:
+            new_val = self.val - other.val
+            new_der = self.der - other.der
+        except:
+            new_val = self.val - other
+            new_der = self.der - other
+
+        return Var(new_val, new_der)
 
     def __rsub__(self, other):
-        raise NotImplementedError
+        return self.__sub__(other)
 
     def __mul__(self, other):
-        raise NotImplementedError
+        try:
+            new_val = self.val * other.val
+            new_der = self.der * other.val + self.val * other.der
+        except:
+            new_val = self.val * other
+            new_der = self.der * other
+        return Var(new_val, new_der)
 
     def __rmul__(self, other):
-        raise NotImplementedError
+        return self.__mul__(other)
 
     def __div__(self, other):
         raise NotImplementedError
