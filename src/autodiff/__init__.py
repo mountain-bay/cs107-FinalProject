235b6b25750d21a738c4505a8d213565c15b1564
zacharyellison <zellison@college.harvard.edu>
2020-11-19 15:07:14 -0700

    package name

--- src/autodiff/__init__.py
+++ src/autodiff/__init__.py
@@ -1,5 +1,5 @@
 from .AD_Object import Var
-from .AD_BasicMath import sin,cos,tan,sqrt,exp,log,ln
+from .AD_BasicMath import sin, cos, tan, sqrt, exp, log, ln
 
 from ._version import get_versions
 __version__ = get_versions()['version']
